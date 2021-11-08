015087a1073ac8acd7355ed551a7d659c7ea1e0b
ssunpa <bestpa.1130@gmail.com>
2021-11-08 15:56:51 +0900

    리팩토링 - 기능 단위로 함수 구현

--- main.js
+++ main.js
@@ -2,43 +2,64 @@ const listBox = document.querySelector('.list_box');
 const textBox = document.querySelector('.add_txt');
 const addBox = document.querySelector('#add_form');
 
-let cnt = 0;
-
 addBox.addEventListener('submit', (event) => {
+    event.preventDefault();
+    addList();
+    textBox.value = '';
+});
+
+listBox.addEventListener('click', (event) => {
+    if (event.target.className == 'fa fa-close') {
+        deleteList(event.target);
+    }
+    if (event.target.tagName == 'INPUT') {
+        checkList(event.target);
+    }
+});
+
+// 리스트 삭제
+function deleteList(target) {
+    target.parentElement.remove();
+}
+
+// 리스트 체크
+function checkList(target) {
+    if (target.checked) {
+        target.nextSibling.style.textDecoration = 'line-through';
+    } else {
+        target.nextSibling.style.textDecoration = 'none';
+    }
+}
+
+// 리스트 추가
+function addList() {
     if (textBox.value) {
-        const todo = document.createElement('input');
-        todo.setAttribute('type', 'checkbox');
-        todo.setAttribute('id', `ch${cnt}`);
-        todo.onclick = function () {
-            if (this.checked) {
-                todolabel.style.textDecoration = 'line-through';
-            } else {
-                todolabel.style.textDecoration = 'none';
-            }
-        };
-
-        const todolabel = document.createElement('label');
-        todolabel.setAttribute('for', `ch${cnt}`);
-        todolabel.textContent = textBox.value;
-
-        const todolist = document.createElement('div');
-        todolist.setAttribute('class', 'list');
-
-        const tododelete = document.createElement('i');
-        tododelete.setAttribute('class', `fa fa-close`);
-        tododelete.onclick = function () {
-            this.parentElement.remove();
-        };
-
-        listBox.appendChild(todolist);
-        todolist.appendChild(todo);
-        todolist.appendChild(todolabel);
-        todolist.appendChild(tododelete);
-
-        cnt++;
-        textBox.value = '';
+        const list = createList(textBox.value);
+        listBox.appendChild(list);
     } else {
         alert('입력된 내용이 없어요😅');
     }
-    event.preventDefault();
-});
+}
+
+// 리스트 html형식 만들기
+function createList(txt) {
+    const todo = document.createElement('input');
+    todo.setAttribute('type', 'checkbox');
+    todo.setAttribute('id', `${Date.now()}`);
+
+    const todolabel = document.createElement('label');
+    todolabel.setAttribute('for', `${Date.now()}`);
+    todolabel.textContent = txt;
+
+    const todolist = document.createElement('div');
+    todolist.setAttribute('class', 'list');
+
+    const tododelete = document.createElement('i');
+    tododelete.setAttribute('class', `fa fa-close`);
+
+    todolist.appendChild(todo);
+    todolist.appendChild(todolabel);
+    todolist.appendChild(tododelete);
+
+    return todolist;
+}
